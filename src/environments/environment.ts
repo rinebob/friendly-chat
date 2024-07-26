f3c84a9ceaf9fb0cf8f1569477dedc4e86b4e403
rinebob <rinebob111185@gmail.com>
2024-07-25 17:11:01 -0700

    [admin] 3 - Added Firebase credentials directly to environment.ts instead of in secrets.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,12 @@
-import { firebaseConfig } from "src/secrets/secrets";
 
 export const environment = {
-  firebase: firebaseConfig,
+  firebase: {
+    apiKey: "AIzaSyDuThReI4wowdREMSXoY2accQWbCpAGHz4",
+    authDomain: "friendly-chat-d71b3.firebaseapp.com",
+    projectId: "friendly-chat-d71b3",
+    storageBucket: "friendly-chat-d71b3.appspot.com",
+    messagingSenderId: "114123465994",
+    appId: "1:114123465994:web:5746584a5c9120c47006f3",
+    locationId:"us-west1",
+  }
 };
