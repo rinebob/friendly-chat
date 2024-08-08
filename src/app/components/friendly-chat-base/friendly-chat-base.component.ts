03ae67c5ed33225c13e4b099714ec43e73ea3b22
rinebob <rinebob111185@gmail.com>
2024-08-08 16:50:29 -0700

    [app] 4 - Continue implementation of Home component with courses and lessons CRUD methods; Adds properties to FriendlyChatBase component

--- src/app/components/friendly-chat-base/friendly-chat-base.component.ts
+++ src/app/components/friendly-chat-base/friendly-chat-base.component.ts
@@ -1,7 +1,9 @@
 import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { toObservable } from '@angular/core/rxjs-interop';
 import { Course, Lesson } from 'src/app/common/interfaces';
 import { COURSES, LESSONS } from 'src/app/common/mock-data';
 import { CoursesService } from 'src/app/services/courses.service';
+import { FriendlyChatStore } from 'src/app/store/friendly-chat-store';
 
 @Component({
   selector: 'app-friendly-chat-base',
@@ -13,10 +15,14 @@ import { CoursesService } from 'src/app/services/courses.service';
 })
 export class FriendlyChatBaseComponent {
 
+    friendlyChatStore = inject(FriendlyChatStore);
     coursesService = inject(CoursesService);
 
     
-    courses = signal<Course[]>(COURSES);
-    lessons = signal<Lesson[]>(LESSONS);
+    initialCourses = signal<Course[]>(COURSES);
+    initialLessons = signal<Lesson[]>(LESSONS);
+
+    courses$ = toObservable(this.friendlyChatStore.courseEntities);
+    lessons$ = toObservable(this.friendlyChatStore.lessonEntities);
 
 }
