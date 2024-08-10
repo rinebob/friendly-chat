8c30c956b0280e6fdc5f1fdf3adf8ff4dcd595d7
rinebob <rinebob111185@gmail.com>
2024-08-10 14:30:01 -0700

    [courses] 3 - Implements CoursesPage and CoursesList components; Adds nav buttons to HeaderComponent Adds effect to FriendlyChatBase component

--- src/app/components/courses-list/course-card/course-card.component.ts
+++ src/app/components/courses-list/course-card/course-card.component.ts
@@ -5,7 +5,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './course-card.component.html',
-  styleUrl: './course-card.component.css',
+  styleUrl: './course-card.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CourseCardComponent {
--- src/app/pages/courses-page/courses-page.component.ts
+++ src/app/pages/courses-page/courses-page.component.ts
@@ -1,13 +1,21 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import {MatTabsModule} from '@angular/material/tabs';
+import { CoursesListComponent } from 'src/app/components/courses-list/courses-list.component';
+import { FriendlyChatBaseComponent } from 'src/app/components/friendly-chat-base/friendly-chat-base.component';
 
 @Component({
   selector: 'app-courses-page',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatIconModule, MatTabsModule, CoursesListComponent],
   templateUrl: './courses-page.component.html',
-  styleUrl: './courses-page.component.css',
+  styleUrl: './courses-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CoursesPageComponent {
+export class CoursesPageComponent extends FriendlyChatBaseComponent implements OnInit {
 
+    ngOnInit(): void {
+        this.getAllCoursesListener();
+    }
 }
